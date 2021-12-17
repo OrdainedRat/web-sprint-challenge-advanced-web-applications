6fc3d8a9a6cc09cef931e95ff4ee024182b47dfe
OrdainedRat <marcusjuniusbrutus213@gmail.com>
2021-12-17 05:29:44 -0600

    pre-review

--- src/components/Article.test.js
+++ src/components/Article.test.js
@@ -25,11 +25,11 @@ const associateArticle = {
 };
 
 test('renders component without errors', ()=> {
-    render(<Article article={[]} />)
+    render(<Article article={[]} />);
 });
 
 test('renders headline, author from the article when passed in through props', ()=> {
-    render(<Article article={testArticle} />)
+    render(<Article article={testArticle} />);
     
     const headline = screen.queryByText(/test headline/i);
     const author = screen.queryByText(/test author/i);
@@ -40,9 +40,9 @@ test('renders headline, author from the article when passed in through props', (
 });
 
 test('renders "Associated Press" when no author is given', ()=> {
-    render(<Article article={associateArticle} />)
+    render(<Article article={associateArticle} />);
 
-    const associates = screen.queryByText(/Associated Press/)
+    const associates = screen.queryByText(/Associated Press/);
     
     
     
@@ -56,10 +56,10 @@ test('executes handleDelete when the delete button is pressed', ()=> {
     
     render(<Article article={testArticle} handleDelete={mockHandleDelete} />)
 
-    const button = screen.queryAllByTestId('deleteButton')
-    userEvent.click(button[0])
+    const button = screen.queryAllByTestId('deleteButton');
+    userEvent.click(button[0]);
 
-    expect(mockHandleDelete.mock.calls.length).toBe(1)
+    expect(mockHandleDelete.mock.calls.length).toBe(1);
 
 });
 
--- src/components/View.test.js
+++ src/components/View.test.js
@@ -2,10 +2,47 @@ import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import View from './View';
 
+
+
 test("renders zero articles without errors", async () => {
+    <View articles={[]} />
+
+    const articles = screen.queryByTestId('article');
+
+    expect(articles).toBeNull();
 });
 
+//I'm not sure what I'm supposed to be mocking or how to.
+
 // test("renders three articles without errors", async ()=> {
+//     render(<View articles={[
+//         {
+//             id:"testid",
+//             headline: "test headline",
+//             author: "test author",
+//             summary: "test summary",
+//             body: "test body"
+//         }, 
+//         {
+//             id:"testid",
+//             headline: "test headline",
+//             author: "test author",
+//             summary: "test summary",
+//             body: "test body"
+//         }, 
+//         {
+//             id:"testid",
+//             headline: "test headline",
+//             author: "test author",
+//             summary: "test summary",
+//             body: "test body"
+//         }, 
+//     ]} />)
+
+//     await waitFor(() => {
+//         const articles = screen.queryAllByTestId('article');
+//         expect(articles).toHaveLength(3);
+//     })    
 // });
 
 //Task List
