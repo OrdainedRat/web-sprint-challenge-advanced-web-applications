de11124b20258c334e4d5a0694b8aca361fe1d37
OrdainedRat <marcusjuniusbrutus213@gmail.com>
2021-12-17 04:50:02 -0600

    article tests completed

--- src/components/Article.test.js
+++ src/components/Article.test.js
@@ -5,18 +5,63 @@ import userEvent from '@testing-library/user-event';
 import MutationObserver from 'mutationobserver-shim';
 
 import Article from './Article';
+import { render, screen, waitFor} from '@testing-library/react';
+
+
+const testArticle = {
+    id:"testid",
+    headline: "test headline",
+    author: "test author",
+    summary: "test summary",
+    body: "test body"
+};
+
+const associateArticle = {
+    id:"testid",
+    headline: "test headline",
+    author: "",
+    summary: "test summary",
+    body: "test body"
+};
 
 test('renders component without errors', ()=> {
+    render(<Article article={[]} />)
 });
 
-// test('renders headline, author from the article when passed in through props', ()=> {
-// });
+test('renders headline, author from the article when passed in through props', ()=> {
+    render(<Article article={testArticle} />)
+    
+    const headline = screen.queryByText(/test headline/i);
+    const author = screen.queryByText(/test author/i);
 
-// test('renders "Associated Press" when no author is given', ()=> {
-// });
+    expect(headline).toBeInTheDocument();
+    expect(author).toBeInTheDocument();
 
-// test('executes handleDelete when the delete button is pressed', ()=> {
-// });
+});
+
+test('renders "Associated Press" when no author is given', ()=> {
+    render(<Article article={associateArticle} />)
+
+    const associates = screen.queryByText(/Associated Press/)
+    
+    
+    
+    expect(associates).toBeInTheDocument();
+    
+   
+});
+
+test('executes handleDelete when the delete button is pressed', ()=> {
+    const mockHandleDelete = jest.fn();
+    
+    render(<Article article={testArticle} handleDelete={mockHandleDelete} />)
+
+    const button = screen.queryAllByTestId('deleteButton')
+    userEvent.click(button[0])
+
+    expect(mockHandleDelete.mock.calls.length).toBe(1)
+
+});
 
 //Task List:
 //1. Complete all above tests. Create test article data when needed.
